c27335d34ce66554b8da37cdc3405beafc0d8a63
Vladislav Valeev <soulproxy0404@gmail.com>
2019-10-07 22:21:25 +0300

    Php подключения переделаны

--- registration.php
+++ registration.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <title>Регистрация</title>
-  
 
 <?php
 require ("header.html");
